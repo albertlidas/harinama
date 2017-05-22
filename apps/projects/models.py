bf1696e1a1495533980b38eb2c7fecbb76962135
Albert Li <ali@lvivsoft.com>
2017-05-22 17:32:15 +0300

    add template with static

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+import datetime
+
 
 # Create your models here.
+class Project(models.Model):
+    """ Project model"""
+
+    title = models.CharField(max_length=200)
+    data = models.DateField(default=datetime.date(2000, 1, 1))
+
+    def __str__(self):
+        return self.title
--- harinama/urls.py
+++ harinama/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^pricing', TemplateView.as_view(template_name="pricing.html")),
+
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
